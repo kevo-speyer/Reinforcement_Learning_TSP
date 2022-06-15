04b8ed501be392ef4fc31ab4d30f62c6e6f6d98a
Kevin Speyer <kevin.speyer@distillery.com>
2022-06-15 17:27:12 -0300

    Added a new train_model function for debugging puropses

--- train.py
+++ train.py
@@ -28,3 +28,36 @@ def train_model(dist_mat, n_train = 1000, gamma = 0.8, alpha = 0.5):
         epsilon = 1. - i * 1/n_train
 
     return q
+
+
+
+def train_model_debug(dist_mat, n_train = 1000, gamma = 0.8, alpha = 0.5):
+    """Same as train_model, bu also returns total distance vs iteration number"""
+    #n_train = 1000 # number of training trips
+    #gamma = 0.8 # Bellman's Parameter for Reinforcement Learning
+    #dist_mat = create_dist_matrix(n_dim = n_dest)
+    n_dest =  dist_mat.shape[0] # number of cities to visit
+    q = np.zeros([n_dest,n_dest]) # transition matrix to train
+    epsilon = 1.
+    distances = []
+    for i in range(n_train):
+        state = [0] # initial state = wharehouse
+        posible_actions = get_avail_act(state, n_dest)
+        distance_traveled = 0
+
+        while posible_actions: # until all destinations are visited
+            action = get_next_action(q, posible_actions, state, epsilon) # np.random.choice(posible_actions)
+            distance_traveled += dist_mat[state[-1], action]
+            q = update_q(q, dist_mat, gamma, state, action, alpha)
+            state.append(action)
+            posible_actions = get_avail_act(state, n_dest)
+
+        # Last trip: from last destination to wharehouse
+        action = 0
+        distance_traveled += dist_mat[state[-1], action]
+        q = update_q(q, dist_mat, gamma, state, action, alpha)
+        state.append(0)
+        epsilon = 1. - i * 1/n_train
+        distances.append(distance_traveled)
+
+    return q, distances
